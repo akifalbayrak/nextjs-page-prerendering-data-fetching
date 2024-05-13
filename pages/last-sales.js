62891aa8523a0a136a737f59c181cdaf28ba5cd2
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-13 21:21:14 +0300

    chore: Update package dependencies with swr 2.2.5

---
+++ pages/last-sales.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function LastSalesPage() {
+    const [sales, setSales] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const { data, error } = useSWR(
+        "https://nextjs-prerendering-fetch-default-rtdb.firebaseio.com/sales.json"
+    );
+
+    useEffect(() => {
+        if (data) {
+            const transformedSales = [];
+
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume,
+                });
+            }
+            setSales(transformedSales);
+        }
+    }, [data]);
+
+    // useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch(
+    //         "https://nextjs-prerendering-fetch-default-rtdb.firebaseio.com/sales.json"
+    //     )
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             const transformedSales = [];
+
+    //             for (const key in data) {
+    //                 transformedSales.push({
+    //                     id: key,
+    //                     username: data[key].username,
+    //                     volume: data[key].volume,
+    //                 });
+    //             }
+    //             setSales(transformedSales);
+    //             setIsLoading(false);
+    //         });
+    // }, []);
+
+    if (error) {
+        return <p>Error Occured!</p>;
+    }
+
+    if (!data || !sales) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <ul>
+            {sales.map((sale) => (
+                <li key={sale.id}>
+                    {sale.username} - ${sale.volume}
+                </li>
+            ))}
+        </ul>
+    );
+}
