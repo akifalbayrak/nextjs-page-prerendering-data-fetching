5f7ea0e1beec344a1105c2fe4028729d99346b1b
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-13 17:25:56 +0300

    first commit

---
+++ pages/[pid].js
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import fs from "fs/promises";
+export default function ProductDetailPage(props) {
+    const { loadedProduct } = props;
+
+    if (!loadedProduct) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <Fragment>
+            <h1>{loadedProduct.title}</h1>
+            <p>{loadedProduct.description}</p>
+        </Fragment>
+    );
+}
+
+export async function getStaticProps(context) {
+    const { params } = context;
+
+    const productId = params.pid;
+
+    const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    const product = data.product((product) => product.id === productId);
+
+    return {
+        props: {
+            loadedProduct: product,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { pid: "p1" } },
+            { params: { pid: "p2" } },
+            { params: { pid: "p3" } },
+        ],
+        fallback: false,
+    };
+}
